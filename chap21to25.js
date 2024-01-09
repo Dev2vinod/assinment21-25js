3155607865291057aa25207c2b02d4cfef07fde1
VINOD SINGH <vinodsinghfun@gmail.com>
2024-01-09 07:37:08 +0530

    Add files via upload

---
+++ chap21to25.js
@@ -0,0 +1,73 @@
+// 1. Write a program that takes two user inputs for first and  last name using prompt and merge them in a new variable  titled fullName. Greet the user using his full name. 
+//  var firstName =prompt("Enter the first name")
+//  var lastName =prompt("enter the last name")
+//  var fullName =firstName+lastName
+//  console.log("welcome to you " +fullName+"  in my site")
+
+//Q2 Write a program to take a user input about his favorite  mobile phone model. Find and display the length of user  input in your browser
+
+
+//  var mobile =prompt("enter your favroti name")
+//  console.log("length of your mobile "+mobile.length)
+
+//Q3  Write a program to find the index of letter “n” in the word “Pakistani” and display the result in your browser . 
+
+//  var word ="Pakistani"
+//  console.log(word.indexOf("n"))
+
+ //Q4 Write a program to find the last index of letter “l” in the  word “Hello World” and display the result in your browser.
+
+//   var text ="Hello World"
+ 
+//   console.log(text.lastIndexOf("l"))
+
+ //Q 5 Write a program to find the character at 3rd index in the  word “Pakistani” and display the result in your browser. 
+  
+//     var word = "Pakistani"
+//     console.log(word.charAt(3))
+
+ //Q6 Repeat Q1 using string concat() method.
+
+//    var firstName =prompt("Enter the first name")
+//   var lastName =prompt("enter the last name")
+//   console.log(firstName.concat(lastName))
+
+ // Q7 Write a program to replace the “Hyder” to “Islam” in the  word “Hyderabad” and display the result in your browser. 
+//    var word ="Hyderabad"
+//    console.log(word.replace(Hyder,Islam))
+
+// Q8  Write a program to replace all occurrences of “and” in the string with “&” and display the result in your browser. var message = “Ali and Sami are best friends. They play cricket and  football together.”; 
+
+// var message =  "Ali and Sami are best friends. They play cricket and  football together";
+ 
+// console.log(message.replace("and,&"))
+
+// Q9 Write a program that converts a string “472” to a number  472. Display the values & types in your browser
+
+//   var str ="472"
+
+//   console.log( str.typeof(str))
+
+//Q10  Write a program that takes user input. Convert and  show the input in capital letters. 
+//  var show =prompt("enter something ")
+//  console.log(show+ "  your capital letter  "+show.toLocaleUpperCase())
+
+// Q 11Write a program that converts the variable num to  string. 
+// var num = 35.36 ; 
+// Remove the dot to display “3536” display in your browser.
+
+//  var num =35.36;
+//  var number  = 3536;
+//  console.log(num.toString())
+//  console.log(typeof(num))
+
+
+ var r ="vinod"
+ for(var i =0 ;i<r.length;i++)
+ {
+      if()
+ }
+
+
+
+
